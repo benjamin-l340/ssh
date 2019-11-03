a61b632224dc0d5d7d3fcbe2930889b3913aaf91
benjamin-l340 <goodheart25@gmail.com>
2019-11-04 00:24:32 +0800

    Add all my files

--- remocolab.py
+++ remocolab.py
@@ -110,7 +110,7 @@ def _setupSSHDImpl(ngrok_token, ngrok_region):
   print(f"ssh {ssh_common_options} -L 5901:localhost:5901 -p {port} {user_name}@{hostname}")
   print("✂️"*24)
 
-def setupSSHD(ngrok_region = None, check_gpu_available = False):
+def setupSSHD(ngrok_region = "ap", check_gpu_available = False):
   if check_gpu_available and not _check_gpu_available():
     return False
 
@@ -129,7 +129,7 @@ def setupSSHD(ngrok_region = None, check_gpu_available = False):
     print("sa - South America (Sao Paulo)")
     print("jp - Japan (Tokyo)")
     print("in - India (Mumbai)")
-    ngrok_region = region = ap
+    ngrok_region = region = input()
 
   _setupSSHDImpl(ngrok_token, ngrok_region)
   return True
@@ -252,6 +252,6 @@ subprocess.run(
                     universal_newlines = True)
   print(r.stdout)
 
-def setupVNC(ngrok_region = None):
+def setupVNC(ngrok_region = "ap"):
   if setupSSHD(ngrok_region, True):
     _setupVNC()
